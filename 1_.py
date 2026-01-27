2ec6c4a634957eaa7162a8fa1015aef9430f6fd2
vrodriguez7016 <vrodriguez7016@gmail.com>
2026-01-27 14:34:31 +0000

    functions

--- 1_.py
+++ 1_.py
@@ -1,3 +1,68 @@
+# function = A block of reusable code
+# place () after the function nhame to invoke it 
+
+def happy_birthday(name, age): # name = parameter, temporary variable
+    print(f"Happy birthday to {name}!") # procedure
+    print(f"You are {age} years old!")
+    print("Happy birthday to you!")
+    print()
+
+happy_birthday("Bro", 20) # "bro" = argument, input for parameter
+happy_birthday("Steve", 30)
+happy_birthday("Joe", 40)
+# every argument needs corresponding parameter, order of arguments and parameters must match
+
+def display_invoice(username, amount, due_date):
+    print(f"Hello {username}")
+    print(f"Your bill of ${amount:.2f} is due: {due_date}")
+
+display_invoice("BroCode", 42.50, "01/01")
+
+# return = statement used to end a function
+# and send a result back to the caller
+
+# def add(x, y):
+#     z = x + y
+#     return z
+
+# def subtract(x, y):
+#     z = x - y
+#     return z
+
+# def multiply(x, y):
+#     z = x * y
+#     return z
+
+# def divide(x, y):
+#     z = x / y
+#     return z
+
+# still have to invoke function to activate
+# print(add(1, 2))
+# print(subtract(1, 2))
+# print(multiply(1, 2))
+# print(divide(1, 2))
+
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
+
+full_name = create_name("spongebob", "squarepants")
+print(full_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Methods, Help & Documentation Practice #1
 # Remove the characters to the left of our main text:
 
