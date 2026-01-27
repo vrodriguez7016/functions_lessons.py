56c267a2cecd4c509075ec4330b14ef4b4ce7361
vrodriguez7016 <vrodriguez7016@gmail.com>
2026-01-27 09:04:34 -0600

    Update 3_return.py

--- 3_return.py
+++ 3_return.py
@@ -7,12 +7,6 @@ def power(a, b):
 
 print(power(1, 21))
 
-
-
-
-
-
-
 # Return Practice #2
 # Create a function called usd_to_eur that takes a numeric value (an amount in US dollars) as its only parameter, and returns the equivalent amount in euros as a result. For the purposes of this example, we will take the conversion 1 USD = 0.90 EUR.
 
